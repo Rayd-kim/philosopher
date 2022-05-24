7089854aedc33fdd0b56859c10a730f67f23af8f
김유성 <kimyousung@gim-yuseong-ui-MacBookAir.local>
2022-05-24 23:04:26 +0900

    0524

---
+++ philosopher.h
@@ -0,0 +1,11 @@
+#ifndef PHILOSOPHER_H
+# define PHILOSOPHER_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <stdlib.h>
+# include <string.h>
+
+#endif
