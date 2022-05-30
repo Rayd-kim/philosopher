ff3badae971d79baf145a587c7154b2d0553cfc1
김유성 <kimyousung@gim-yuseong-ui-MacBookAir.local>
2022-05-30 21:59:41 +0900

    0530

--- philosopher.h
+++ philosopher.h
@@ -16,17 +16,37 @@ typedef struct	s_philo
 	int			eat_time;
 	int			sleep_time;
 	int			eat_num;
+	int			left;
+	int			right;
 }		t_philo;
 
 typedef struct s_all
 {
 	int				num;
 	int				philo_num;
-	int				*fork;
+	int				eat_num;
+	int				*fork_num;
 	struct timeval	start;
-	pthread_mutex_t	*mutex_fork;
+	pthread_mutex_t	*fork;
 	struct s_philo	*philo;
 }		t_all;
+
+/*
+typedef struct	s_list
+{
+	int		*k;
+	int		num;
+	int		left;
+	int		right;
+	struct s_thread	*thread;
+}		t_list;
+
+typedef struct s_thread
+{
+	struct s_list	*list;
+	pthread_mutex_t	*mutex;
+}		t_thread;
+*/
 
 int	ft_atoi(const char *str);
 
--- test2.c
+++ test2.c
@@ -1,73 +1,71 @@
 #include "philosopher.h"
 
+/*
 typedef struct	s_list
 {
-	char	*p;
 	int		*k;
 	int		num;
-	pthread_mutex_t	*mutex;
+	int		left;
+	int		right;
+	struct s_thread	*thread;
 }		t_list;
 
-
-typedef struct	s_thread
+typedef struct s_thread
 {
 	struct s_list	*list;
+	pthread_mutex_t	*mutex;
 }		t_thread;
+*/
 
 void	*t_funtion(void *data)
 {
-	pthread_t	tid;
+	t_list		*list;
+	t_thread	*thread;
 
-	tid = pthread_self();
-	t_list	*list;
 	list = (t_list *)data;
-	printf("%d\n", pthread_mutex_lock(list->mutex));
-	for (int i = 1; i < 6; i++)
-	{
-		list->k[list->num] += 1;
-		printf ("[%s] --- %d\n", list->p, list->k[list->num]);
-		usleep(1000*1000);
-		printf ("1 더해짐\n");
-	}
-	pthread_mutex_unlock(list->mutex);
+	thread = list->thread;
+	pthread_mutex_lock(&thread->mutex[list->left]);
+	pthread_mutex_lock(&thread->mutex[list->right]);
+	printf ("%d 스레드가 %d 포크를 들었다.\n", list->num, list->left);
+	printf ("%d 스레드가 %d 포크를 들었다.\n", list->num, list->right);
+	usleep (1000 * 2000);
+	pthread_mutex_unlock(&thread->mutex[list->left]);
+	pthread_mutex_unlock(&thread->mutex[list->right]);
 	return (0);
 }
 
 int main(void)
 {
-    pthread_t	a[2];
+	int		n = 10;
+    pthread_t	a[n];
     int			thr_id;
 	int			status;
+	int			b = 0;
 
-	pthread_mutex_t	mutex;
-	
-	//pthread_mutex_init(&mutex, NULL);
-	char	*p[2];
-	p[0] = "thread_1";
-	p[1] = "thread_2";
-	int		b[2] = {0, 0};
+	//memset (b, 0, sizeof(int) * n);
+	t_thread	*thread = malloc(sizeof(t_thread));
+	thread->mutex = malloc (sizeof(pthread_mutex_t) * n);
+	thread->list = (t_list *)malloc(sizeof(t_list) * n);
 
-	t_thread	*thread = malloc(sizeof(t_thread) * 2);
-	thread[0].list = (t_list *)malloc(sizeof(t_list));
-	thread[1].list = (t_list *)malloc(sizeof(t_list));
 
-	for (int i = 0; i < 2; i++)
+	for (int i = 0; i < n; i++)
 	{
-		thread[i].list->num = i;
-		thread[i].list->p = p[i];
-		thread[i].list->k = b;
-		thread[i].list->mutex = &mutex;
-	}
-	pthread_mutex_init(thread[0].list->mutex, NULL);
-	
-	for (int i = 0; i < 2; i++)
-	{
-		thr_id = pthread_create(&a[i], NULL, t_funtion, (void *)thread[i].list);
-		//usleep(1000 * 1000);
+		thread->list[i].num = i;
+		thread->list[i].k = &b;
+		thread->list[i].thread = thread;
+		thread->list[i].left = i;
+		thread->list[i].right = (i + 1) % n;
+		pthread_mutex_init(&thread->mutex[i], NULL);
 	}
 	
-	pthread_join(a[0], (void **)&status);
-	pthread_join(a[1], (void **)&status);
+	for (int i = 0; i < n; i++)
+	{
+		thr_id = pthread_create(&a[i], NULL, t_funtion, (void *)&thread->list[i]);
+		usleep (50);
+	}
+	
+	for (int i = 0; i < n; i++)
+		pthread_join(a[i], (void **)&status);
 	printf("program_end\n");
 	return 0;
 }
